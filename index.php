eb0655eaf96b621eea28ad8cfb439018e07b1a2f
Bryan Bibat <bry@bryanbibat.net>
2014-10-26 19:04:01 +0800

    Revert "Test container wrapping fix"
    This reverts commit 024dcc604e5b5ea6d81dd7ac931c94b98b2038bd.

--- index.php
+++ index.php
@@ -104,9 +104,7 @@
 		    </div>
 		</div>
       </div>
-    </div>
-  <div class="container">
-  <div class="row">
+    </div>	
 	<div class="spacer"></div>
 	<div id="home" class="page-section">
 		<div class="banner-home">
@@ -135,9 +133,7 @@
 				</div>
 			</div>
 		</div>
-  </div><!-- /banner-home -->
-  </div>
-  <div class="row">
+	</div><!-- /banner-home -->
 	<div id="about" class="page-section">
 		<div class="spacer"></div>
 		<div class="header-wrap">
@@ -171,8 +167,6 @@
 		</div>
 		<div class="spacer"></div>
 	</div><!-- /banner-home -->
-  </div>
-  <div class="row">
 	<div id="howToJoin" class="page-section">
 		<div class="spacer"></div>
 		<div class="header-wrap">
@@ -217,8 +211,6 @@
 		</div>
 		<div class="spacer"></div>
 	</div><!-- /banner-home -->
-  </div>
-  <div class="row">
 	<div id="mechanics" class="page-section">
 		<div class="spacer"></div>
 		<div class="header-wrap">
@@ -278,8 +270,6 @@
 		</div>
 		<div class="spacer"></div>
 	</div><!-- /banner-home -->
-  </div>
-  <div class="row">
 	<div id="sponsors">
 		<div class="spacer"></div>
 		<div class="header-wrap">
@@ -306,8 +296,6 @@
         <iframe src="//www.facebook.com/plugins/likebox.php?href=https%3A%2F%2Fwww.facebook.com%2Fdevcondevise&amp;width&amp;height=427&amp;colorscheme=light&amp;show_faces=false&amp;header=true&amp;stream=true&amp;show_border=true&amp;appId=488047311326879" scrolling="no" frameborder="0" style="border:none; overflow:hidden; height:427px; width:100%" allowTransparency="true"></iframe>
       </div>
     </div>
-  </div>
-  </div>
   </div>
 	<footer>
 		<div class="footer-wrap">
